c73c19ccb825a36ee3f6d18ca336551af9b57766
wassimwali <wassimwali700@gmail.com>
2025-01-28 20:53:24 +0100

    defintion de la classe Gaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,13 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	public Gaulois (String nom , int force) {
+		this.nom=nom
+		this.force=force
+	}
+	public getNom() {
+			return nom;
+	}
+}
